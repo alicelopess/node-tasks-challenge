0ffad426afbc78bbb83f8a08738c4f77e1ce4dbc
Maria Alice <alicedflopes.02@gmail.com>
2024-04-13 10:49:26 -0300

    feat: add base project

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,34 @@
+import { randomUUID } from 'node:crypto'
+
+//TODO: Create temporary database
+const tasks = []
+
 //TODO: Create routes array
 export const routes = [
     {
         method: 'GET',
         path: '/tasks',
         handler: (request, response) => {
+            console.log(tasks)
+
             return response
                 .setHeader('content-type', 'application/json')
-                .end('Tasks List...')
+                .end(`Tasks List: ${tasks.length === 0 ? '[]' : JSON.stringify(tasks)}`)
         }
     },
     {
         method: 'POST',
         path: '/tasks',
         handler: (request, response) => {
+            tasks.push({
+                id: randomUUID(),
+                title: 'Um título',
+                description: 'Uma descrição',
+                completed_at: null,
+                created_at: 'Data de criação',
+                updated_at: 'Data de atualização'
+            })
+
             return response
                 .writeHead(201)
                 .end('Task Created!')
